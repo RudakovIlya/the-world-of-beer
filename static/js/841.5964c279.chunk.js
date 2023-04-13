961cb82edd145cd916ba0e5dbbca2d86ce446b6f
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-04-13 13:25:47 +0300

    Updates

---
+++ static/js/841.5964c279.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 841.5964c279.chunk.js.LICENSE.txt */
+"use strict";(globalThis.webpackChunkthe_world_of_beer=globalThis.webpackChunkthe_world_of_beer||[]).push([[841],{8841:(e,t,o)=>{o.r(t),o.d(t,{startStatusTap:()=>a});var r=o(1554),n=o(3743),s=o(1811);const a=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,r.wj)((()=>{const t=e.innerWidth,o=e.innerHeight,a=document.elementFromPoint(t/2,o/2);if(!a)return;const i=(0,n.a)(a);i&&new Promise((e=>(0,s.c)(i,e))).then((()=>{(0,r.Iu)((async()=>{i.style.setProperty("--overflow","hidden"),await(0,n.s)(i,300),i.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=841.5964c279.chunk.js.map
